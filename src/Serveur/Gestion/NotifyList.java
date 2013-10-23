4f0211bc99ecac85e3a91a77d4e618fe79fc17f5
Maxence Adnot <adnot.maxence@gmail.com>
2013-10-23 15:51:46 +0200

    Valenti n a demandé un push

--- src/Serveur/Gestion/NotifyList.java
+++ src/Serveur/Gestion/NotifyList.java
@@ -3,31 +3,35 @@ package Serveur.Gestion;
 import java.util.ArrayList;
 
 import org.jdom2.Document;
+import org.jdom2.Element;
 
 public abstract class NotifyList<E> extends ArrayList<E> {
 	
-	private static final long serialVersionUID = 1L;
-
-	private Document document;
+	protected Document document;
+	protected Element racine;
+	
+	public abstract void addToXML(E _o);
+	public abstract void removeFromXML(Object _o);
+	protected abstract Element getElementFromId(String _id);
 	
 	public int getSize()
 	{
 		return super.size();
 	}
 	
-	public boolean remove(Object o)
+	public boolean remove(Object _o)
 	{
-		this.removeFromXML();
-		return super.remove(o);
+		this.removeFromXML(_o);
+		return super.remove(_o);
 	}
 	
-	public boolean add(E o)
+	public boolean add(E _o)
 	{
-		this.addToXML();
-		return super.add(o);
+		if(this.contains(_o))
+		{
+			this.addToXML(_o);
+			return super.add(_o);
+		}
+		return false;
 	}
-	
-	abstract void addToXML();
-	abstract void removeFromXML();
-	
 }
