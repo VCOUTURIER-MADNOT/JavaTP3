bcaf9005cc46c8fef19f4351602259162d5cd1fc
coutcout <valcou@aliceadsl.fr>
2013-10-27 01:36:33 +0200

    Implémentation des méthodes pour Forum
    Il reste à corriger le getElementFromId car le e.getChildren ne regarde
    que le level inférieur à e et n'est pas récursif.
    Il faut donc parcourir ses enfants (les forums) et regarder leur sujet
    et non parcourir directement les sujets (cf. UtilisateurNotifyList)

--- src/Serveur/NotifyLists/ForumNotifyList.java
+++ src/Serveur/NotifyLists/ForumNotifyList.java
@@ -1,23 +1,45 @@
 package Serveur.NotifyLists;
 
+import java.io.File;
+import java.rmi.RemoteException;
 import java.util.Iterator;
 import java.util.List;
 
 import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 
 import Serveur.Classes.Forum;
-import Serveur.Classes.Personne;
 
 public class ForumNotifyList extends NotifyList<Forum>{
 
+	public ForumNotifyList()
+	{
+		SAXBuilder sxb = new SAXBuilder();
+		this.xmlUrl = "Forums.xml";
+		try
+		{
+			this.document = sxb.build(new File(this.xmlUrl));
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		this.racine = this.document.getRootElement();
+		for (Element eForum : this.racine.getChildren("Forum"))
+		{
+			this.add((Forum) this.getObjectFromElement(eForum), false);
+		}
+	}
+	
 	@Override
-	public void addToXML(Forum _o) {
-		Element eForum = new Element("Forum");
-		
-		Element eSujet = new Element("Sujet");
-		eSujet.setText(_o.getSujet());
-		
-		eForum.addContent(eSujet);
+	public void addToXML(Forum _f) {
+		Element eForum = this.getElementFromObject(_f);
+		if (eForum != null)
+		{
+			this.racine.addContent(eForum);
+			System.out.println("Forum " + _f.getSujet() + " inséré dans la liste.");
+		}
 		
 		this.racine.addContent(eForum);
 	}
@@ -46,13 +68,29 @@ public class ForumNotifyList extends NotifyList<Forum>{
 
 	@Override
 	protected Element getElementFromObject(Object _o) {
-		// TODO Auto-generated method stub
-		return null;
+		Element eForum = null;
+		
+		if(_o instanceof Forum)
+		{
+			Forum f = (Forum) _o;
+			eForum = new Element("Forum");
+			
+			Element eSujet = new Element("Sujet");
+			eSujet.setText(f.getSujet());
+		
+			eForum.addContent(eSujet);
+		}
+		
+		return eForum;
 	}
 
 	@Override
 	protected Object getObjectFromElement(Element _e) {
-		// TODO Auto-generated method stub
+		try {
+			Forum f = new Forum(_e.getChildText("Sujet"));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
